eb20129b5eb2f671dabbd5cb701460634c4a21c2
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-21 09:49:39 +0100

    structure the project

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.c                                          :+:      :+:    :+:   */
+/*   ft_push_swap.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/20 08:47:14 by anfouger          #+#    #+#             */
-/*   Updated: 2025/11/21 09:46:08 by anfouger         ###   ########.fr       */
+/*   Created: 2025/11/21 09:36:27 by anfouger          #+#    #+#             */
+/*   Updated: 2025/11/21 09:45:01 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-int	ft_parsing(int ac, char **av, t_stack **stack_a, t_stack **stack_b)
+void	ft_push_swap(int ac, char **av)
 {
-	if (!verif_arg(ac, av))
-		return (NULL);
-	ft_create_stack_a(ac, av, &(*stack_a));
-	if (ft_is_same(&(*stack_a)))
-	{
-		free(*stack_a);
-		free(*stack_b);
-		return (NULL);
-	}
-	ft_create_stack_b(&(*stack_b), (*stack_a)->size);
-	return (1);
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	
+	stack_a = malloc(sizeof(t_stack));
+	if (!stack_a)
+        return (NULL);
+	stack_b = malloc(sizeof(t_stack));
+	if (!stack_b)
+        return (NULL);
+	ft_parsing(ac, av, &stack_a, &stack_a);
 }
