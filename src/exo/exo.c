5a2acfa1dc15a2ba2753649b8fbeb8802ee585c2
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-24 14:58:54 +0100

    ex02 push

--- src/exo/exo.c
+++ src/exo/exo.c
@@ -18,14 +18,55 @@ void    initStack(Stack *stack)
     stack->top = NULL;
 }
 
-int main(void)
+Node	*createNode(int value)
+{
+	Node *node = malloc(sizeof(Node));
+	if (!node)
+		return (NULL);
+    node->value = value;
+	node->next = NULL;
+	return (node);
+}
+
+void    push(Stack **stack, int value)
+{
+	Node *node;
+	Node *tmp;
+
+	node = createNode(value);
+	if (!(*stack)->top)
+	{
+		(*stack)->top = node;
+		return ;
+	}
+	tmp = (*stack)->top;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = node;
+}
+
+int main(int ac, char **av)
 {
     Stack *stack;
+	Node	*tmp;
+    int		i = 1;
 
     stack = malloc(sizeof(Stack));
     initStack(stack);
     if (stack->top == NULL)
         printf("%s", "stack is init\n");
+    while (i < ac)
+    {
+		push(&stack, atoi(av[i]));	
+		i++;
+    }
+	tmp = stack->top;
+	printf("%s", "stack apres push:\n");
+	while (tmp)
+	{
+		printf("%d\n", tmp->value);
+		tmp = tmp->next;
+	}
     free(stack);
     return 0;
 }
