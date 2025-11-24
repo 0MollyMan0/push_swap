821c40a2d745c8e0c73689f352a63fb10118732b
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-24 11:58:30 +0100

    free issue corrected

--- src/parsing/ft_parsing.c
+++ src/parsing/ft_parsing.c
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 08:47:14 by anfouger          #+#    #+#             */
-/*   Updated: 2025/11/24 11:22:56 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/11/24 11:58:04 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,23 @@
 
 char	**ft_parsing(int ac, char **av)
 {
-	int i;
-	char *join;
-	char **split;
+	int     i;
+	char    *join;
+	char    *tmp;
+	char    **split;
 
 	i = 1;
-	join = NULL;
 	join = ft_strjoin("", av[i]);
 	while (i++ < ac - 1)
-		join = ft_strjoin(join, av[i]);
+	{
+		tmp = ft_strjoin(join, av[i]);
+		free(join);
+		join = tmp;
+	}
 	split = ft_split(join, ' ');
+	free(join);
 	if (!split)
 		return (NULL);
-	free(join);
 	if (!ft_verif_num(split) || !ft_verif_same(split))
 	{
 		free_tab(split);
