a8d5f5f95023d4b6d08a5ea225e3cdb338fd9e8d
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-25 15:26:22 +0100

    swap function done but not tested

--- src/push_swap_command/swap.c
+++ src/push_swap_command/swap.c
@@ -6,7 +6,48 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 11:36:17 by anfouger          #+#    #+#             */
-/*   Updated: 2025/11/25 11:36:18 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/11/25 15:23:25 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../push_swap.h"
+
+void	sa(t_stack **stack)
+{
+	int tmp;
+	
+	if (!(*stack) || !(*stack)->top || (*stack)->size < 2)
+        return ;
+    tmp = (*stack)->top->data;
+	(*stack)->top->data = (*stack)->top->next->data;
+	(*stack)->top->next->data = tmp;
+	write(1, "sa\n", 3);
+}
+
+void	sb(t_stack **stack)
+{ 
+	int tmp;
+	
+	if (!(*stack) || !(*stack)->top || (*stack)->size < 2)
+        return ;
+    tmp = (*stack)->top->data;
+	(*stack)->top->data = (*stack)->top->next->data;
+	(*stack)->top->next->data = tmp;
+	write(1, "sb\n", 3);
+}
+
+void	ss(t_stack **stack_a, t_stack **stack_b)
+{
+	int	tmp;
+	
+	if (!(*stack_a) || !(*stack_a)->top || (*stack_a)->size < 2
+		|| !(*stack_b) || !(*stack_b)->top || (*stack_b)->size < 2)
+        return ;
+    tmp = (*stack_a)->top->data;
+	(*stack_a)->top->data = (*stack_a)->top->next->data;
+	(*stack_a)->top->next->data = tmp;
+	tmp = (*stack_b)->top->data;
+	(*stack_b)->top->data = (*stack_b)->top->next->data;
+	(*stack_b)->top->next->data = tmp;
+	write(1, "ss\n", 3);
+}
