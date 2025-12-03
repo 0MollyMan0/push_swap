ab67bf2d01b0b0752c93564fc4060347a58a3242
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-12-03 16:05:01 +0100

    end of the day

--- src/algo/ft_sort.c
+++ src/algo/ft_sort.c
@@ -6,21 +6,33 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 16:03:44 by anfouger          #+#    #+#             */
-/*   Updated: 2025/12/03 11:31:03 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/12/03 16:04:29 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
 
-void	ft_sort(t_stack *stack_a, t_stack *stack_b)
+void	ft_sort(t_stack **stack_a, t_stack **stack_b)
 {
+	t_chunk	*chunks;
 	if ((*stack_a)->size < 5)
-		ft_sort_small(&(*stack_a), &(*stack_b));
-	
+		ft_sort_small(&(*stack_a), &(*stack_b), &chunks);
 }
 
-void	ft_first_part(t_stack **stack_a, t_stack **stack_b)
+void	ft_first_part(t_stack **stack_a, t_stack **stack_b, t_chunk **chunks)
 {
+	t_node	*tmp;
+	t_node	*node;
+
+	node = (*stack_a)->top;
+	while ((*stack_a)->size != 0)
+	{
+		if (node->index >= (*chunks)->start && node->index <= (*chunks)->end)
+		{
+			/* code */
+		}
+		
+	}
 	
 }
