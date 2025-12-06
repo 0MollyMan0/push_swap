eecf7cf5e19d6a3f32fbeb3b516313cd2d96110e
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-12-06 15:25:33 +0100

    sort finished

--- src/algo/ft_sort.c
+++ src/algo/ft_sort.c
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 16:03:44 by anfouger          #+#    #+#             */
-/*   Updated: 2025/12/05 11:44:42 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/12/06 12:28:01 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,46 @@
 void	ft_sort(t_stack **stack_a, t_stack **stack_b)
 {
 	t_chunk	*chunks;
+	int		count;
 	
 	if ((*stack_a)->size <= 5)
+	{
 		ft_sort_small(stack_a, stack_b);
+		return ;
+	}
 	else
 	{
 		chunks = ft_chunking((*stack_a)->size);
 		ft_first_part(stack_a, stack_b, &chunks);
 	}
+	count = (*stack_b)->size - 1;
+	while (count != 0)
+	{
+		ft_bring_top(stack_b, count);
+		pa(stack_a, stack_b);
+		count--;
+	}
 }
+
+// void	ft_first_part(t_stack **stack_a, t_stack **stack_b, t_chunk **chunks)
+// {
+// 	t_node	*node;
+// 	t_chunk *chunk;
+
+// 	chunk = (*chunks);
+	
+// 	while ((*stack_a)->size != 0)
+// 	{
+// 		node = (*stack_a)->top;
+// 		ft_bring_chunk_top(stack_a, chunk->start, chunk->end);
+// 		pb(stack_a, stack_b);
+// 		chunk->count++;
+// 		if ((*stack_b)->size > 1 && (*stack_b)->top->index % 2 == 0)
+// 			rb(stack_b);
+// 		if (chunk->count == chunk->size)
+// 			chunk = chunk->next;
+// 	}
+// }
 
 void	ft_first_part(t_stack **stack_a, t_stack **stack_b, t_chunk **chunks)
 {
--- src/main.c
+++ src/main.c
@@ -6,103 +6,103 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 09:25:37 by anfouger          #+#    #+#             */
-/*   Updated: 2025/12/04 12:15:47 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/12/06 13:18:58 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+// #include <stdio.h>
 #include <push_swap.h>
 
-static void	print_stack_data(t_stack	*stack)
-{
-	t_node	*tmp;
-	int		i;
+// static void	print_stack_data(t_stack	*stack)
+// {
+// 	t_node	*tmp;
+// 	int		i;
 	
-	if (!stack)
-	{
-		printf("%s", "stack est NULL");
-		return ;
-	}
-	if (!stack->top)
-	{
-		printf("%s", "rien dans cette stack\n");
-		return ;
-	}
-	tmp = stack->top;
-	i = 0;
-	while (i < stack->size)
-	{
-		printf("data = %d\n", tmp->data);
-		tmp = tmp->next;
-		i++;
-	}
-}
-static void	print_stack_index(t_stack	*stack)
-{
-	t_node	*tmp;
-	int		i;
+// 	if (!stack)
+// 	{
+// 		printf("%s", "stack est NULL");
+// 		return ;
+// 	}
+// 	if (!stack->top)
+// 	{
+// 		printf("%s", "rien dans cette stack\n");
+// 		return ;
+// 	}
+// 	tmp = stack->top;
+// 	i = 0;
+// 	while (i < stack->size)
+// 	{
+// 		printf("data = %d\n", tmp->data);
+// 		tmp = tmp->next;
+// 		i++;
+// 	}
+// }
+// static void	print_stack_index(t_stack	*stack)
+// {
+// 	t_node	*tmp;
+// 	int		i;
 	
-	if (!stack)
-	{
-		printf("%s", "stack est NULL");
-		return ;
-	}
-	if (!stack->top)
-	{
-		printf("%s", "rien dans cette stack\n");
-		return ;
-	}
-	tmp = stack->top;
-	i = 0;
-	while (i < stack->size)
-	{
-		printf("index = %d\n", tmp->index);
-		tmp = tmp->next;
-		i++;
-	}
-}
+// 	if (!stack)
+// 	{
+// 		printf("%s", "stack est NULL");
+// 		return ;
+// 	}
+// 	if (!stack->top)
+// 	{
+// 		printf("%s", "rien dans cette stack\n");
+// 		return ;
+// 	}
+// 	tmp = stack->top;
+// 	i = 0;
+// 	while (i < stack->size)
+// 	{
+// 		printf("index = %d\n", tmp->index);
+// 		tmp = tmp->next;
+// 		i++;
+// 	}
+// }
 
 int main(int ac, char **av)
 {
 	char	**split;
 	t_stack	*stack_a;
 	t_stack	*stack_b;
-	t_chunk *chunk;
+	// t_chunk *chunk;
 	int 	i;
 
-	printf("%s", "\n/*----------Debut de ce test-----------*/\n");
+	// printf("%s", "\n/*----------Debut de ce test-----------*/\n");
 	split = ft_parsing(ac, av);
 	if (!split)
 		return (0);
 	i = 0;
-	while (split[i] && split)
-	{
-		printf("split %d = %s\n", i, split[i]);
-		i++;
-	}
+	// while (split[i] && split)
+	// {
+	// 	printf("split %d = %s\n", i, split[i]);
+	// 	i++;
+	// }
 	stack_a = ft_create_stack_a(split);
 	free_tab(split);
 	stack_b = ft_create_stack_b();
-	printf("\n%s\n", "Stack_a:");
-	print_stack_data(stack_a);
-	printf("\n%s\n", "Stack_b:");
-	print_stack_data(stack_b);
+	// printf("\n%s\n", "Stack_a:");
+	// print_stack_data(stack_a);
+	// printf("\n%s\n", "Stack_b:");
+	// print_stack_data(stack_b);
 
-	printf("%s", "\n/*----Test Indexation A----*/\n\n");
+	// printf("%s", "\n/*----Test Indexation A----*/\n\n");
 	ft_indexation(stack_a);
-	printf("\n%s\n", "Stack_a:");
-	print_stack_index(stack_a);
-	printf("\n%s\n", "Stack_b:");
-	print_stack_index(stack_b);
+	// printf("\n%s\n", "Stack_a:");
+	// print_stack_index(stack_a);
+	// printf("\n%s\n", "Stack_b:");
+	// print_stack_index(stack_b);
 
-	printf("%s", "\n/*----Test Chunking----*/\n\n");
-	chunk = ft_chunking(stack_a->size);
-	t_chunk *tmp = chunk;
-	while (tmp)
-	{
-		printf("chunk start = %d  chunk end = %d  chunk size = %d\n", tmp->start, tmp->end, tmp->size);
-		tmp = tmp->next;
-	}
+	// printf("%s", "\n/*----Test Chunking----*/\n\n");
+	// chunk = ft_chunking(stack_a->size);
+	// t_chunk *tmp = chunk;
+	// while (tmp)
+	// {
+	// 	printf("chunk start = %d  chunk end = %d  chunk size = %d\n", tmp->start, tmp->end, tmp->size);
+	// 	tmp = tmp->next;
+	// }
 	
 	// printf("%s", "\n/*----Test 3 Push A vers B----*/\n\n");
 	// pb(&stack_a, &stack_b);
@@ -113,15 +113,15 @@ int main(int ac, char **av)
 	// printf("\n%s\n", "Stack_b:");
 	// print_stack_data(stack_b);
 	
-	printf("%s", "\n/*----Test sort----*/\n\n");
+	// printf("%s", "\n/*----Test sort----*/\n\n");
 	ft_sort(&stack_a, &stack_b);
-	printf("\n%s\n", "Stack_a:");
-	print_stack_index(stack_a);
-	printf("\n%s\n", "Stack_b:");
-	print_stack_index(stack_b);
+	// printf("\n%s\n", "Stack_a:");
+	// print_stack_index(stack_a);
+	// printf("\n%s\n", "Stack_b:");
+	// print_stack_index(stack_b);
 	
-	printf("%s", "\n/*----------Fin de ce test-----------*/\n\n");
-	ft_free_chunk(chunk);
+	// printf("%s", "\n/*----------Fin de ce test-----------*/\n\n");
+	// ft_free_chunk(chunk);
 	ft_free_stack(stack_a);
 	ft_free_stack(stack_b);
 	
